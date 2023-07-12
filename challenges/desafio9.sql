2d5eacda60c6232a64e1b9ba64b7de4bd19a6c80
Paulo Victor Belens Moreira <paulovictor55296@gmail.com>
2023-07-11 22:27:54 -0300

    corrigindo o 9º req

--- challenges/desafio9.sql
+++ challenges/desafio9.sql
@@ -1,5 +1,5 @@
 SELECT 
 COUNT(*) AS musicas_no_historico
 FROM musicas_ouvidas AS mo
-INNER JOIN pessoas_usuarias AS pu ON pu.usuario_id = mo.usuario_id
-WHERE pu.usuario_nome = 'Barbara Liskov';
+INNER JOIN pessoas_usuarias AS pu ON pu.pessoa_usuaria_id = mo.pessoa_usuaria_id
+WHERE pu.pessoa_usuaria_nome = 'Barbara Liskov';
